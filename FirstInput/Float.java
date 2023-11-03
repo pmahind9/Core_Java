77788c0dac7ba66513f541bcce0af0f918fba887
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-03 17:43:25 +0530

    First_input

---
+++ FirstInput/Float.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+class FirstInputString{
+
+
+     public static void main (String [] ram)
+     {
+         
+              
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your name");
+String name=sc.nextLine();
+System.out.println("Enter your email");
+String name=sc.nextLine();
+
+		System.out.println("name: "+name);
+		System.out.println("name: "+email);
+         
+         
+    }
+
+ }
