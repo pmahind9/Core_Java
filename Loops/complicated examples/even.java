85a23fb151277ce90a35f588c047e9445bd8e20f
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-15 12:50:06 +0530

    Add files via upload

---
+++ Loops/complicated examples/even.java
@@ -0,0 +1,18 @@
+/*
+write a java program to input first and last number and print all even number between them
+*/
+ import java.util.Scanner;
+class Forloop{
+public static void main(String[] args){
+
+Scanner sc=new Scanner(System.in);
+System.out.println("ENter your firsr & Last number");
+int first=sc.nextInt();
+int last=sc.nextInt();
+
+for(int i=first;i<=last;i++){
+if(i%2==0){
+System.out.println(i);
+}
+}
+}}
---
+++ Loops/complicated examples/table.java
@@ -0,0 +1,25 @@
+//For Loop
+
+import java.util.Scanner;
+
+class Table
+{
+
+	public static void main(String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	  System.out.println("table of");
+	  
+ 	  
+	    int n=sc.nextInt(); 
+	  
+	                        
+	  for( int i=1; i<=10; i++)			   
+		{
+		 System.out.println(i*n);  
+		 			   
+		}
+
+	}
+
+}
