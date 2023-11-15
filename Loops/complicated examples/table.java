e0689851b88babe82ab31ddbb2cf0f8226218392
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-15 12:55:34 +0530

    Update table.java

--- Loops/complicated examples/table.java
+++ Loops/complicated examples/table.java
@@ -1,25 +1,15 @@
 //For Loop
-
 import java.util.Scanner;
-
 class Table
 {
-
 	public static void main(String[] args)
 	{
 	 Scanner sc=new Scanner(System.in);
 	  System.out.println("table of");
-	  
- 	  
-	    int n=sc.nextInt(); 
-	  
-	                        
+	    int n=sc.nextInt();                       
 	  for( int i=1; i<=10; i++)			   
 		{
-		 System.out.println(i*n);  
-		 			   
+		 System.out.println(i*n);  	   
 		}
-
 	}
-
 }
