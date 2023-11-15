e96c98b7ea763d9ec04a84e6794aa8beb1996564
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-15 12:55:57 +0530

    Update armstrong.java

--- Loops/examples for understand/armstrong.java
+++ Loops/examples for understand/armstrong.java
@@ -1,23 +1,16 @@
-
-
 import java.util.Scanner;
-
 class Armstrong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-  
         int sum = 0;
 
         for (int i= n; i!= 0; i=i/10) {
-            
-
 	    int rem=i%10;
             sum = sum + (rem * rem * rem);
         }
-
         if (n == sum) {
             System.out.println(n + " is  Armstrong number.");
         } else {
