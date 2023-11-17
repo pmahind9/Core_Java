0325cc0b053dbf55ecb33c2aba8f570d23027d0e
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-17 17:03:38 +0530

    Update q9.java

--- Loops/Digit count examples/q9.java
+++ Loops/Digit count examples/q9.java
@@ -1,20 +1,16 @@
 /*
 Q9.Write a program to input a 5-digit number and find the largest digit in it.
 */
-import java.util.Scanner;
 
+import java.util.Scanner;
 class Five{
 public static void main(String[] args){
 
 	Scanner ref=new Scanner(System.in);
 	System.out.println("Enter Number");
-	
-
 	int n=ref.nextInt();
 	int temp=n;
 	int l=0;
-	
-
 	while(temp!=0)
 	{
 	int rem=temp%10;
