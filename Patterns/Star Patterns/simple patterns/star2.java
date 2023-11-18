2751a49caf61e0363527427859f86662e7fe9496
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-18 16:34:16 +0530

    Update star2.java

--- Patterns/Star Patterns/simple patterns/star2.java
+++ Patterns/Star Patterns/simple patterns/star2.java
@@ -1,8 +1,8 @@
 class Star2{
 public static void main(String args[]){
 
-int n=5;
-
+	int n=5;
+	
 	for(int i=1; i<=n; i++){
 		
 	for (int j=1; j<=i; j++){
@@ -10,7 +10,6 @@ int n=5;
 
 	}
 	System.out.println();
-
 }
 
 }
