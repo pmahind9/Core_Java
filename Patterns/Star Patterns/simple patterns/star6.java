52af45bcb455daf31474efd67d206492497df076
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-18 16:33:02 +0530

    Add files via upload

---
+++ Patterns/Star Patterns/simple patterns/star6.java
@@ -0,0 +1,22 @@
+class Star6{
+public static void main(String args[]){
+
+int n=9;
+
+	for(int i=n; i>=1; i--){
+		
+	for (int j=i; j<n; j++){
+
+		System.out.print(" ");
+
+	}
+	for (int k=1; k<=i; k++){
+
+	System.out.print("* ");
+	}
+	  System.out.println();
+	
+	}
+
+}
+}
---
+++ Patterns/Star Patterns/simple patterns/star7.java
@@ -0,0 +1,39 @@
+class Star7{
+public static void main(String args[]){
+
+int col=9;
+
+	
+	for(int i=1; i<=9; i++){
+		
+	for (int j=i; j<col; j++){
+
+		System.out.print(" ");
+
+	}
+	for (int k=1; k<=i; k++){
+
+	System.out.print("* ");
+	}
+	  System.out.println();
+	
+	}
+
+
+	for(int i=col; i>=1; i--){
+		
+	for (int j=i; j<col; j++){
+
+		System.out.print(" ");
+
+	}
+	for (int k=1; k<=i; k++){
+
+	System.out.print("* ");
+	}
+	  System.out.println();
+	
+	}
+
+}
+}
