b51762487c0dccfc83a5eb8a956be801e7b38b50
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-18 16:34:39 +0530

    Update star5.java

--- Patterns/Star Patterns/simple patterns/star5.java
+++ Patterns/Star Patterns/simple patterns/star5.java
@@ -6,9 +6,9 @@ class star5{
 	
 	for (int i=1; i<=n; i++){
 		
-		for (int j=i; j<n; j++){
-			System.out.print(" ");
-
+		for (int j=i; j<n; j++)
+		{
+	     	 System.out.print(" ");
 		}
 	   for (int k=1; k<=i; k++){
 		 System.out.print("* ");
