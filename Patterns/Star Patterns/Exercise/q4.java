39062b079d0829df498ba25fc53bd82d4d552e6e
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-20 12:10:43 +0530

    Update q4.java

--- Patterns/Star Patterns/Exercise/q4.java
+++ Patterns/Star Patterns/Exercise/q4.java
@@ -18,7 +18,6 @@ public class Star4 {
             for (int j = 1; j < i; j++) {
                 System.out.print(" ");
             }
-
             for (int k = i; k <= n; k++) {
                 System.out.print("* ");
             }
