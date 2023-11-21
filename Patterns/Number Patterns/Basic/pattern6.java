3bb93dd88f65313fc2ae0948cb4de3fcd62e0465
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-21 11:40:37 +0530

    Update pattern6.java

--- Patterns/Number Patterns/Basic/pattern6.java
+++ Patterns/Number Patterns/Basic/pattern6.java
@@ -8,18 +8,15 @@ public static void main(String[] args){
 		for(int j=1; j<=i; j++){
 	
 		System.out.print(j+" ");
-	
 	}
 	 System.out.println();
 }
 	for(int i=1; i<=5; i++){
 
 		for(int j=1; j<=i; j++){
-	
 		System.out.print(j+" ");
-	
 	}
 	 System.out.println();
-}
-}
+	}
+    }
 }
