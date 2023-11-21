90941d2bd9909dee726b6a92cfbb245a2c3e5a8c
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-21 11:39:57 +0530

    Update number_pattern_12.java

--- Patterns/Number Patterns/Basic/number_pattern_12.java
+++ Patterns/Number Patterns/Basic/number_pattern_12.java
@@ -1,21 +1,13 @@
  class NumberPattern12 {
     public static void main(String[] args) {
-
 	int a;
-
        for (int i = 1; i <= 5; i++) {
-
 		a=i;
-
             for (int j = i; j >= 1; j--) {
-
         System.out.print(a + " ");
 		a=a+5;
     }
     System.out.println();
 }
-
-
-
     }
 }
