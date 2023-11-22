9463efe7d3f44188f1d97fec83f6394112ce6aed
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-22 12:41:48 +0530

    Update alpha_pattern3.java

--- Patterns/Alpha Patterns/alpha_pattern3.java
+++ Patterns/Alpha Patterns/alpha_pattern3.java
@@ -2,33 +2,20 @@
 
 class Alphapattern3{
   public static void main(String[] args){
-
-	
-
 	for(char i='A'; i<='F'; i++){
 	 
 	  for (char j='A'; j<=i; j++){
 
 		System.out.print(j+ " ");
-
 	   }
-
 		System.out.println();
-
 	}
-
 		for(char i='F'; i>='A'; i--){
-	 
 	  for (char j='A'; j<i; j++){
-
 		System.out.print(j+ " ");
-
 	   }
-
 		System.out.println();
-
 	}
-
   }
 }
 
