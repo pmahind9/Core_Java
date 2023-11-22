623f86ba354ae87af24cac39354dd4ccdfa4af72
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-22 12:42:06 +0530

    Update alphapattern1_logic2.java

--- Patterns/Alpha Patterns/alphapattern1_logic2.java
+++ Patterns/Alpha Patterns/alphapattern1_logic2.java
@@ -2,20 +2,11 @@
 
 class Alphapattern1{
   public static void main(String[] args){
-
-	
-
 	for(int i=65; i<=69; i++){
-	 
 	  for (int j=65; j<=i; j++){
-
 		System.out.print((char)(i)+ " " );
-
 	   }
-
 		System.out.println();
-
 	}
-
   }
 }
