78beee6034baf99bd3771c299a65881b9413be60
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-23 12:15:17 +0530

    Update p4.java

--- Patterns/Alpha Patterns/p4.java
+++ Patterns/Alpha Patterns/p4.java
@@ -2,19 +2,12 @@
 
 class Alphapatterna{
   public static void main(String[] args){
-
 	int num=64;
-
 	for(int i=6; i>1; i--){
-	 
 	  for (int j=1; j<=i; j++){
-
 		System.out.print((char)(num+j) + " ");
-
 	   }
-
 		System.out.println();
-
 	}
 
 		for(int i=1; i<=6; i++){
@@ -22,11 +15,8 @@ class Alphapatterna{
 	  for (int j=1; j<=i; j++){
 
 		System.out.print((char)(num+j) + " ");
-
 	   }
-
 		System.out.println();
-
 	}
 
   }
