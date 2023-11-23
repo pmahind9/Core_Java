6e8c0957c95b0ac1e670e2c305fb521a1f182c49
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-23 12:14:15 +0530

    Update p8.java

--- Patterns/Alpha Patterns/p8.java
+++ Patterns/Alpha Patterns/p8.java
@@ -9,24 +9,14 @@ F E D C B A
 
 */
 
-
 class Alphapattern8{
   public static void main(String[] args){
-
 	int num=71;
-
 	for(int i=1; i<=6; i++){
-	 
 	  for (int j=1; j<=i; j++){
-
 		 System.out.print((char)(num-j)+" " );
-
 	   }
-	    
 	    System.out.println();
-
 	}
-
-}
-
+    }
 }
