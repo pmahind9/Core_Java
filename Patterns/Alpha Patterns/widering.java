94f85d6000b3010b8c271a7a567bfb06c421b9ab
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-23 12:13:35 +0530

    Update widering.java

--- Patterns/Alpha Patterns/widering.java
+++ Patterns/Alpha Patterns/widering.java
@@ -2,13 +2,8 @@
 
 class Windering{
  public static void main(String [] args){
-
 	long l=10;
 	int a= (int)l;  //if u dont use (int) then its get error
-
 	System.out.println(a);
-
-
-}
-
+    }
 }
