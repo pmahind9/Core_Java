4932d7e34a43e8b0fbb540770fd8109c6f7198ec
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-23 12:15:42 +0530

    Update p14.java

--- Patterns/Alpha Patterns/p14.java
+++ Patterns/Alpha Patterns/p14.java
@@ -9,27 +9,17 @@ E D C B A
 
 */
 
-
 class Alphapattern14{
   public static void main(String[] args){
-
 	int num=64;
-
 	for(int i=1; i<=6; i++){
-	 
 	  for (int j=i; j<=1; j++){
-
 		 System.out.print((char)(num+j)+" " );
-
 	   }
 		for(int j=i-1; j>=1; j--){
 			System.out.print((char)(num+j)+" " );
 		}
-	    
 	    System.out.println();
-
 	}
-
-}
-
+    }
 }
