dfbd281f48a5b3e7de0018a701cf9db6f077e348
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-24 15:45:42 +0530

    Update p7.java

--- Patterns/Assignments/p7.java
+++ Patterns/Assignments/p7.java
@@ -12,21 +12,13 @@ F
 
 class Alphapattern7{
   public static void main(String[] args){
-
 	int num=71;
-
 	for(int i=6; i>=1; i--){
-	 
 	  for (int j=1; j<=i; j++){
-
 		 System.out.print((char)(num-j)+" " );
-
 	   }
-	    
 	    System.out.println();
-
 	}
-
-}
+    }
 
 }
