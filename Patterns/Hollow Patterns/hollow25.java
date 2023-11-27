0069efc5d7c4debd868ab1dd9252d8679dcc541a
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-27 15:22:33 +0530

    Update hollow25.java

--- Patterns/Hollow Patterns/hollow25.java
+++ Patterns/Hollow Patterns/hollow25.java
@@ -11,26 +11,17 @@
 
 class Hollow25{
 public static void main(String[] args){
-
 	for(int i=1; i<=5; i++){
-
 		for(int j=1; j<=i; j++){
-		
 		System.out.print(" ");
 		}
-
 			for(int k=1; k<=5; k++){
-
-			
-			
 			if(i==1 || i==5 || k==1 || k==5){
 				 System.out.print("*");
 				}
 				else{
 				System.out.print(" ");
 				}
-
-
 			}
 			
 
