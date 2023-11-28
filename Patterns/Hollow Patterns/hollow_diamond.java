57c5dea1a3ad699e236881c91f3df07d6e46b6bb
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-28 17:37:08 +0530

    Update hollow_diamond.java

--- Patterns/Hollow Patterns/hollow_diamond.java
+++ Patterns/Hollow Patterns/hollow_diamond.java
@@ -13,16 +13,12 @@
 
 class HollowDiamond{
 public static void main(String[] args){
-
-
 	for (int i=1; i<5; i++)
 	   {
 		   for(int j=5; j>i; j--)
 			{
 			  System.out.print(" ");
 			}
-
-
 	    	  for(int j=1; j<=i; j++)
 		     {
 			if(j==1 || i==j)
@@ -36,9 +32,6 @@ public static void main(String[] args){
 	  	     }
 	       System.out.println();
 	    }
-
-
-
 	for(int i=1; i<=5; i++)
 	  {
 		for(int j=1; j<i; j++)
@@ -55,12 +48,7 @@ public static void main(String[] args){
 		{
 		 System.out.print("  ");
 		}
-
-		
 	     System.out.println();
 	  }
-
-
-
-}
+  }
 }
