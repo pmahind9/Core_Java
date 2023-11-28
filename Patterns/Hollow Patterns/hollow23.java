6458cda4e4fa9b033a2cdbdb5dcca91f331eafa9
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-11-28 17:32:27 +0530

    Update hollow23.java

--- Patterns/Hollow Patterns/hollow23.java
+++ Patterns/Hollow Patterns/hollow23.java
@@ -28,19 +28,15 @@ public static void main(String[] args){
 		}
 	    System.out.println();
 	}
-
 	for (int i=1; i<=5; i++){
-		for(int j=1; j<=5; j++){
-		
+		for(int j=1; j<=5; j++){	
 		if( j==5 || i==j){
 		 System.out.print("* ");
 		}else{
 			System.out.print("  ");
-			
 		    }
 		}
 	    System.out.println();
 	}
-
-}
+   }
 }
