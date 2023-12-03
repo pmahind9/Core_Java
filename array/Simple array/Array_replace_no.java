95b8c666a1041e5fd65449e93dadd13e23972244
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-04 00:00:03 +0530

    Update Array_replace_no.java

--- array/Simple array/Array_replace_no.java
+++ array/Simple array/Array_replace_no.java
@@ -4,7 +4,6 @@ Q3. WAP to replace all the 0’s with 1’s in your array. Your array is
 */
 class Array_Replace{
 public static void main(String [] args){
-
 int n[]= {12,2,0,6,4,0,9,0,8,7,3,0,4};
 
 for(int i=0; i<n.length; i++)
