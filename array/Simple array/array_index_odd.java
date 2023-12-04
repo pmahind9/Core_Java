7318795768f99f0aeb9c6941031aa787cfd62603
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-04 15:45:45 +0530

    Update array_index_odd.java

--- array/Simple array/array_index_odd.java
+++ array/Simple array/array_index_odd.java
@@ -1,15 +1,10 @@
 class Array {
 public static void main(String[] args){
-
-
 int a[]={1,2,3,4,5,6,7,8,9,10};
-
 for (int i=1; i< a.length; i++)
-
 	if(i%2!=0)
 	{
 	 System.out.println("index number is Odd "+i+ " - "+"and element is " +  a[i]);
 	}
-
-}
+   }
 }
