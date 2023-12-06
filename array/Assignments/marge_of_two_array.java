87a7f3a7a5d7f3b84a2d16942f6ac59433e82d47
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-06 16:50:31 +0530

    Update marge_of_two_array.java

--- array/Assignments/marge_of_two_array.java
+++ array/Assignments/marge_of_two_array.java
@@ -1,11 +1,9 @@
 /*merging of two array. hyamde C[i]B.LENGTJ ghetal nahi karn pahila a.length che array ahet na tya 0-5 index position la jr direct ts ghetl ast tr te override zale aste mhnun apn 
 "A.LENGTH+I" ghetl mhnje ki a.length array pasun pudh array put honr.
-
 */
 
 class Merge_of_arrays{
 public static void main(String[] args){
-
 int a[]={9,1,3,5,6,};
 int b[]={11,22,66,10,19};
 int c[]=new int[a.length + b.length];
