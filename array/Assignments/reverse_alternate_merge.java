cb6db7d1ce4a3e438fa32fb2c86993b09097853a
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-09 14:37:59 +0530

    Update reverse_alternate_merge.java

--- array/Assignments/reverse_alternate_merge.java
+++ array/Assignments/reverse_alternate_merge.java
@@ -7,44 +7,25 @@ Q3.a[]={10,20,30,40,50}
 
 class Merge_of_Reverse_arrays{
 public static void main(String[] args){
-
 int a[]={10,20,30,40,50};
 int b[]={1,2,3,4,5};
 int c[]=new int[a.length + b.length];
-
-
 	int x=0;
 	for(int i=0; i<a.length; i++)
 	 {
-		
 		c[x]=a[i];
 		x=x+2;
-
 	 }
-
-
 	int y=1;
 	for(int i=b.length-1; i>=0; i--)   //"-1" nhi kel tr ha error yeto:- Index 10 out of bounds for length 10
 	{
-	
 		c[y]=b[i];
 		y=y+2;
 	}
-
 	 for(int i=0; i<c.length; i++)
 	{
 		System.out.print(c[i]+ " ");
 	}
-
 }
 }
 
-
-
-
-
-
-
-/*
- 
-*/
