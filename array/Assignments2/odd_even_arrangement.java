174df9d332734d9cb9e5899ec5611eb1d6dc6bfd
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-15 21:40:41 +0530

    Update odd_even_arrangement.java

--- array/Assignments2/odd_even_arrangement.java
+++ array/Assignments2/odd_even_arrangement.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 class Odd_even_Arrangement {
     public static void main(String[] args) {
         int a[] = {1,0,6,9,11,8,9,34,7,5,12,};
-
         int b[] = new int[a.length];
 	int x=0;
-
         for (int i = 0; i < a.length ; i++) 
 	{
            if(a[i]%2==0)
