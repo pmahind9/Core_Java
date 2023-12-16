b5f848d3a3241b6c0f5c5b82bde1086b3893a7c7
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-16 14:02:17 +0530

    Update clockwise_direction.java

--- array/Assignments2/clockwise_direction.java
+++ array/Assignments2/clockwise_direction.java
@@ -6,7 +6,6 @@ class ClockwiseDirection {
         for (int i = a.length - 1; i > 0; i--) {
             a[i] = a[i - 1];
         }
-
         a[0] = n; 
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
