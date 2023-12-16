b29e2de667b0f3f6b2e0190a0c5681a5a9fd9c50
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-16 14:02:34 +0530

    Update arrangement.java

--- array/Assignments2/arrangement.java
+++ array/Assignments2/arrangement.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 class Arrangement{
     public static void main(String[] args) {
         int a[] = {1,0,1,0,0,1,-3,1,0,-4,1,0,-2,0};
-
         int b[] = new int[a.length];
 	int x=0;
         for (int i = 0; i < a.length ; i++) 
