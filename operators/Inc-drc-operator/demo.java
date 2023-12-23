3910c0193b41b6e0835fbfd7d012399329f6fde2
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-23 14:08:17 +0530

    Update demo.java

--- operators/Inc-drc-operator/demo.java
+++ operators/Inc-drc-operator/demo.java
@@ -1,33 +1,21 @@
 /* Unary Operator
-
 A. increment opr: incre value by 1
   1.preincreament: it incre value then print
     intk=++x
-
   2. post incream: first print vlaue then increase
     m=y++
-
 . decreament operator: it decreas value by 1.
   1. pre decr (--value)
   2. post deac (value -- )
-
 */
-
-
-
 class Demo 
 {
-
-
      public static void main (String [] ram)
-     {
-         
+     {        
           int x=10;
          System.out.println(x++); //10
 	 System.out.println(x++); //11
-	 System.out.println(++x);
-	
-         
+	 System.out.println(++x);   
     }
 
  }
