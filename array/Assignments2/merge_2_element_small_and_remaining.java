cfdb04fab4f22c9c55e65ae3e71c506506d3fda8
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2023-12-26 10:28:26 +0530

    Update merge_2_element_small_and_remaining.java

--- array/Assignments2/merge_2_element_small_and_remaining.java
+++ array/Assignments2/merge_2_element_small_and_remaining.java
@@ -18,7 +18,6 @@ class Sorted {
         int[] a = { 1, 5, 6, 7, 8, 10 };
         int[] b = { 2, 4, 9 };
         int[] c = new int[a.length + b.length];
-
         for (int i = 0; i < a.length; i++) {
             c[i] = a[i];
         }
