878d497fe1435a11b1ea308d37d24bf5bd419355
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2024-01-01 23:37:37 +0530

    Update matrix_user_input - Copy.java

--- array/Assignment3/matrix_user_input - Copy.java
+++ array/Assignment3/matrix_user_input - Copy.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 class Matrix_user_input{
 public static void main(String [] args){
 	Scanner sc=new Scanner(System.in);
-
 	System.out.println("Enter your Row size");
 	int row=sc.nextInt();
 
