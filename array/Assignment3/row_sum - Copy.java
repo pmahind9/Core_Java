08db1b7bff058f4fc670f71f0d58da23b0e96c08
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2024-01-03 23:52:19 +0530

    Update row_sum - Copy.java

--- array/Assignment3/row_sum - Copy.java
+++ array/Assignment3/row_sum - Copy.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 class RowSum {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter your Row size");
