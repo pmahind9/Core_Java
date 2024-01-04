7ed2168da040cda4962a9b242c9996539cef44b6
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2024-01-04 11:08:24 +0530

    Update matrix_max.java

--- array/Assignment3/matrix_max.java
+++ array/Assignment3/matrix_max.java
@@ -1,34 +1,25 @@
 import java.util.Scanner;
-
 class Matrix_user_input {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter your Row size");
         int row = sc.nextInt();
-
         System.out.println("Enter your Column size");
         int colm = sc.nextInt();
-
         System.out.println("Enter your Matrix Element");
         int a[][] = new int[row][colm];
-
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < colm; j++) {
                 a[i][j] = sc.nextInt();
             }
         }
-
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
                 System.out.print(a[i][j] + " ");
             }
             System.out.println();
         }
-
         int max = a[0][0];
-
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
                 if (a[i][j] > max) {
