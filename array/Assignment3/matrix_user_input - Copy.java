fd985607a66bbd9b0f0bd2be5b8c55945f0fcb8d
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2024-01-05 16:52:05 +0530

    Update matrix_user_input - Copy.java

--- array/Assignment3/matrix_user_input - Copy.java
+++ array/Assignment3/matrix_user_input - Copy.java
@@ -4,14 +4,10 @@ public static void main(String [] args){
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter your Row size");
 	int row=sc.nextInt();
-
 	System.out.println("Enter your COlumn size");
 	int colm=sc.nextInt();
-
 	System.out.println("Enter your Matrix Element");
 	int a[][] = new int [row][colm];
-	
-
 	for (int i=0; i < row ; i++)
 	{
 	 for(int j=0; j < colm; j++)
