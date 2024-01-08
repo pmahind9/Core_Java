b800a8f847ca72c2628c21767b50df95fb5ef11c
pmahind9 <116084360+pmahind9@users.noreply.github.com>
2024-01-08 11:49:15 +0530

    Update Max_no_return_type_without_argument.java

--- Opps/No_return_type_without_Argument/Max_no_return_type_without_argument.java
+++ Opps/No_return_type_without_Argument/Max_no_return_type_without_argument.java
@@ -1,24 +1,17 @@
-
 import java.util.Scanner; 
-
  class Max_no_return_type_without_argument {
 	public void maximum_element() {
-		int a[] = {1,2,13,14,15,6,17,9,8,21,2,8,4};
-		
+		int a[] = {1,2,13,14,15,6,17,9,8,21,2,8,4};	
 		int max = a [0];
-		
 		for (int i = 0; i < a.length; i++) {
 			if (a[i]>max) {
 				max = a[i];
 			}
 		}
-		
 		System.out.println(max);
 	}
-	
 	public static void main (String[] args) {
-		Max_no_return_type_without_argument ob = new Max_no_return_type_without_argument()
-;
-		ob.maximum_element();}
-
+		Max_no_return_type_without_argument ob = new Max_no_return_type_without_argument();
+		ob.maximum_element();
+	}
 }
